fbe09d41a57c52f1d7b49a49ccd7d1dba4a55f9c
IHTFPhacking <IHTFP.mailer@null.net>
2017-01-22 22:33:20 -0600

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -24,8 +24,7 @@ function decryptText()
 	}
 	else if (encrypted == "U2FsdGVkX1/Y1u3x2QUZFplzg3yKQc6Bq12twtLqm9Hgjnwc4WHzbaMYYAQUG9T+AlIj4uxEPSXAorBCBMfi7wYqyw7M5pnaYGKxTnudl6tq2C4LiEdZi33MlL8CKnKdK0edSX4sMXGyPLbUlnzZKt4FlURRDok0agqNEFT3JSmQxPT/qQhw/CcEl07axuxa8v00KDPYD5ipQYZlryrvfNQAgxiKXsKL+eyiBucuJRT2vbFRXWaH4UU5DAS8jJQ1cz8bAKn6c1g0v3NoGfKMRKF4vCDKv+vAXxwB6bv/bf0=")
 	{
-		var decryptedText = decrypted.toString(CryptoJS.enc.Utf8);
-		alert(decryptedText);
+		alert("Well, by now you've probably figured it out. Since I won't be going to MIT, I figured I could bring a piece of it here. Here's to many great hacks at your future school, and a great time here.");
 		changePicture5();
 	}
 }
